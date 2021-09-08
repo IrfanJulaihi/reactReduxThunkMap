98c9ee132576eaa9f9e1897d9a5938711e9c1a82
Mohd Irfan Bin Julaihi <mohdirfan@sains.com.my>
2021-09-08 13:49:13 +0800

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React,{ useState } from "react";
+import { NewNoteInput } from "./NewNoteInput";
+import { useSelector, useDispatch } from "react-redux";
+import { addNote } from "./actions";
+import { saveNotes, loadNotes } from "./notesReducer";
+import GooglePlacesAutocomplete from 'react-google-places-autocomplete'
 function App() {
+  // const YOUR_GOOGLE_MAPS_API_KEY="AIzaSyB_xff7D6Fa4ZyjQaLMuBsU6nrJLEEI3kI"
+  const YOUR_GOOGLE_MAPS_API_KEY="AIzaSyB_xff7D6Fa4ZyjQaLMuBsU6nrJLEEI3kI"
+  const notes = useSelector((state) => state.notes);
+  const dispatch = useDispatch();
+
+  const onAddNote = (note) => {
+  
+    dispatch(addNote(note.label));
+  };
+
+  const onSave = (note) => {
+    dispatch(saveNotes(note));
+  };
+
+  const onLoad = () => {
+    dispatch(loadNotes());
+  };
+  const [value, setValue] = useState();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <GooglePlacesAutocomplete
+  apiKey={YOUR_GOOGLE_MAPS_API_KEY}
+  selectProps={{
+    onChange:onAddNote
+  }}
+  autocompletionRequest={{
+        bounds: [
+      { lat: 50, lng: 50 },
+      { lat: 100, lng: 100 }
+    ],
+    componentRestrictions: {
+    country: ['my'],
+    }
+  }}
+  onLoadFailed={(error) => (
+    console.error("Could not inject Google script", error)
+  )}
+/>
+      <NewNoteInput addNote={onAddNote} />
+      <hr />
+      
+      <br/>
+      <br/>
+      <br/>
+      <br/> <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/> <br/>
+      <br/>
+  
+      <br/>
+      {value}
+
+
+
+      <ul>
+        {notes.map((note) => {
+          return <li key={note}>{note}</li>;
+        })}
+      </ul>
+      <hr />
+     
+      <button onClick={onSave}>Save</button>
+      <button onClick={onLoad}>Load</button>
+    </>
   );
 }
 
---
+++ src/notesReducer.js
@@ -0,0 +1,40 @@
+import { ADD_NOTE, setNotes, SET_NOTES } from "./actions"
+
+const initialState = {
+  notes: []
+}
+
+export const notesReducer = (state = initialState, action) => {
+  switch(action.type){
+    case ADD_NOTE: {
+      return {...state, notes: [...state.notes, action.payload]}
+    }
+    case SET_NOTES: {
+      return {...state, notes: action.payload}
+    }
+    default:
+      return state
+  }
+}
+
+export const saveNotes = () => async (dispatch, getState) => {
+  const notes = getState().notes;
+  // await fetch("http://localhost:4000/notes", {
+  //   method: "POST",
+  //   headers: {
+  //     'Accept': 'application/json',
+  //     'Content-type': 'application/json'
+  //   },
+  //   body: JSON.stringify(notes)
+  // })
+
+  localStorage.setItem('places', JSON.stringify(notes));
+  alert("Success")
+} 
+
+export const loadNotes = () => async (dispatch, getState) => {
+  const notes = JSON.parse(localStorage.getItem('places'));
+
+console.log(notes)
+  dispatch(setNotes(notes))
+} 
